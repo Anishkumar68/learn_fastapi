5fc73df6b0790d2dfe294daa0d2b2e08d29200d3
anishkumar68 <anishkumar231999@gmail.com>
2026-09-14 06:48:53 +0530

    Fastapi: rephrase

--- app/database.py
+++ app/database.py
@@ -1,15 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+# the sqlite path where the file will be created and a connection will be stablized with it 
 DATABASE_URL = "sqlite:///./app.db"
 
+# engine is connection manager with database
+# we are using the sqlite thats why we passed the connect_args 
+engine = create_engine(DATABASE_URL, connect_args= {"check_same_thread" : False})
 
-engine = create_engine(
-    DATABASE_URL, connect_args = {"check_same_thread" : False}
-)
 
-sessionLocal = sessionmaker(bind = engine, autoflush=False, autocommit = False)
+# to store session with database
+sessionLocal = sessionmaker(bind=engine, autocommit =False, autoflush=False)
 
+# dependancy 
 def get_db():
     db = sessionLocal()
 
