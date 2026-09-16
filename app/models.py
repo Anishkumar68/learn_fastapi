47729d29bd8fe499a782cdfea18108bcbf21e5b8
anishkumar68 <anishkumar231999@gmail.com>
2026-09-16 21:00:20 +0530

    Desgined the post,user and comment databsae tables

--- app/models.py
+++ app/models.py
@@ -1,8 +1,29 @@
-from unittest.mock import Base
+from sqlalchemy import DeclarativeBase, Mapped, mapped
 
+class Base(DeclarativeBase):
+    pass
 class User(Base):
     __tablename__ = "user"
 
-    id : Mapped[int] =mapped.int(primary) 
-    Name:Mapped[str] = mapped.str(255)
+    id : Mapped[int] =mapped.int(primary = True) 
+    Name:Mapped[str] = mapped.str(100)
     Email: Mapped[str] = mapped.str(255)
+
+class Post(Base):
+    __tablename__ = "post"
+
+    id:Mapped[int] = mapped.int(primary = True)
+    title:Mapped[str] = mapped.str(200)
+    description:Mapped[str] = mapped.str(500)
+    likes:Mapped[int] = mapped.int()
+    comments:Mapped[int] = mapped.int()
+    longtext:Mapped[str] = mapped.str(1000)
+
+class Comment(Base):
+    __tablename__ = "comment"
+
+    id:Mapped[int] = mapped.int(primary = True)
+    comment:Mapped[str] = mapped.str(500)
+    post_id:Mapped[int] = mapped.int()
+
+
