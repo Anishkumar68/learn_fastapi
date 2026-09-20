ce8e1098cd188c45e8b36170395b71529d4c51c3
anishkumar68 <anishkumar231999@gmail.com>
2026-09-20 21:07:59 +0530

    updated Schema.py

--- app/schema/schema.py
+++ app/schema/schema.py
@@ -2,15 +2,26 @@ import datetime
 from pydantic import BaseModel, ConfigDict, EmailStr, Field
 from app.database import Base
 
-class User(BaseModel):
-    user_name:str
-    email:EmailStr
-    password:str
 
+class UserBase(BaseModel):
+    username:str=Field(min_length = 5, max_length=50)
+    email : EmailStr = Field(max_length = 150)
+class User(UserBase):
+   pass
 class UserResponse(BaseModel):
-    user_name:str
-    email:str
+    id = int
+    img_file : str | None
+    img_path : str
 
+    model_config = ConfigDict(from_attributes = True)
 class PostBase(BaseModel):
     title:str=Field(min_length=1, max_length=100)
     email:EmailStr = Field(max_length=120)
+
+class PostCreate(PostBase):
+    user_id:int
+    
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    id :int
+    auther: UserResponse
